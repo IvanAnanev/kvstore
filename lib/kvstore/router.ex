8d4dc6a9cffed9e8209b2f77dc20e089c6eb292f
Ivan Ananiev <ivcheg@gmail.com>
2017-05-11 14:21:47 +0300

    add verify request for /stores/post

--- lib/kvstore/router.ex
+++ lib/kvstore/router.ex
@@ -2,9 +2,16 @@
 defmodule KVstore.Router do
   use Plug.Router
 
+  alias KVstore.Plug.VerifyRequest
+
+  plug Plug.Parsers, parsers: [:urlencoded, :multipart]
+  plug VerifyRequest, fields: ["key", "value"], paths:  ["/stores"]
+
   plug :match
   plug :dispatch
 
-  get "/", do: send_resp(conn, 200, "Welcome")
+  get "/stores/:key", do: send_resp(conn, 200, "GET")
+  post "/stores", do: send_resp(conn, 200, "POST")
+
   match _, do: send_resp(conn, 404, "Oops!")
 end
