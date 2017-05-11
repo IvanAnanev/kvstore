3410c04702c52b2ebbefd7c52e89703e9f6d5e42
Ivan Ananiev <ivcheg@gmail.com>
2017-05-11 12:54:43 +0300

    setup router and some refactoring

--- lib/kvstore.ex
+++ lib/kvstore.ex
@@ -2,6 +2,13 @@ defmodule KVstore do
   use Application
 
   def start(_type, _args) do
-    KVstore.Supervisor.start_link
+    import Supervisor.Spec
+
+    children = [
+      Plug.Adapters.Cowboy.child_spec(:http, KVstore.Router, [], port: 8080),
+      worker(KVstore.Storage, [KVstore.Storage])
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
--- lib/kvstore/router.ex
+++ lib/kvstore/router.ex
@@ -1,4 +1,10 @@
 # Для веб сервера нужен маршрутизатор, место ему именно тут.
 defmodule KVstore.Router do
+  use Plug.Router
 
+  plug :match
+  plug :dispatch
+
+  get "/", do: send_resp(conn, 200, "Welcome")
+  match _, do: send_resp(conn, 404, "Oops!")
 end
